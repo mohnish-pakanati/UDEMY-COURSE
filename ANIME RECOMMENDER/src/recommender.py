da9118ead172e28aa75304798de996d0a358964c
mohnish-pakanati <p.mohnish02@gmail.com>
2025-10-31 20:22:39 +0530

    recommender

---
+++ ANIME RECOMMENDER/src/recommender.py
@@ -0,0 +1,14 @@
+from langchain.chains import RetrievalQA
+from lanchain_groq import ChatGroq
+from src.prompt_template import get_anime_prompt
+
+# db, prompt template, 
+class AnimeRecommender:
+    def __init__(self, retriever, api_key:str, model_name:str):
+
+        # Initialize prompt template
+        
+        # Initialize groq chat
+        # Initialize retrieverqa
+    
+    # create a class to run qa chain
--- ANIME RECOMMENDER/src/vector_store.py
+++ ANIME RECOMMENDER/src/vector_store.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loader.csv_loader import CSVLoader
 from lanchain_huggingface_embedding import HuggingFaceEmbeddings
@@ -13,4 +13,22 @@ class VectorStoreBuilder:
         self.embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     
     def build_and_save_vectorstore(self):
-        loader = CSVLoader(file)
+        # load, split, embed, and save to disk
+
+        loader = CSVLoader(file_path=self.csv_path, 
+                           encoding='utf-8',
+                           metadata_columns=[])
+        data = loader.load()
+
+        splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = splitter.split_documents(data)
+
+        db = Chroma.from_documents(documents=texts, 
+                                         embedding=self.embedding, 
+                                         persist_directory=self.persist_dir)
+        db.persist()
+
+    def load_vector_store(self):
+        db = Chroma(persist_directory=self.persist_dir, 
+                    embedding_function=self.embedding)
+        return db
